93387c7b6901bbb7ea78974a3ad2313d2c05faca
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-12-12 12:09:10 +0100

    Add plotting and correlation of the values

--- CORRELATION/correlation.py
+++ CORRELATION/correlation.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 f = open ('./track.txt', 'r')
 f2 = open ('./analysis.txt', 'r')
 
@@ -7,6 +9,18 @@ analysis_times = []
 for line in f:
 	track_times.append(line)
 for line in f2:
-	track_times.append(line)
+	analysis_times.append(line)
 
-#make algorithm to sort the lists and the second one
+new_track_times = []
+new_analysis_times = []
+quocients = []
+
+for i in range(350):
+	new_track_times.append(int(track_times[i]))
+	new_analysis_times.append(int(analysis_times[i]))
+	quocients.append(int(track_times[i])/int(analysis_times[i]))
+
+plt.plot(new_analysis_times, new_track_times, 'r*')
+plt.plot(quocients, 'b*')
+plt.axis('equal')
+plt.show()
