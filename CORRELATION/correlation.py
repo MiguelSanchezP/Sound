c058487811901b14583820422f1de4c4bc7c140b
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-12-13 12:58:26 +0100

    Fixed typo in word quotients

--- CORRELATION/correlation.py
+++ CORRELATION/correlation.py
@@ -14,32 +14,32 @@ for line in f2:
 
 new_track_times = []
 new_analysis_times = []
-quocients = []
+quotients = []
 
 for i in range(3501):
 	new_track_times.append(int(track_times[i]))
 	new_analysis_times.append(int(analysis_times[i]))
-	quocients.append(int(track_times[i])/int(analysis_times[i]))
+	quotients.append(int(track_times[i])/int(analysis_times[i]))
 
 plot1 = plt.figure(1)
 plt.plot(new_analysis_times, new_track_times, 'r*')
-plt.plot(quocients, 'b*')
+plt.plot(quotients, 'b*')
 plt.axis('equal')
 plt.xlabel ("Analysis function execution time in ns")
 plt.ylabel ("Additional functions execution time in ns")
 plt.title ("Execution times")
 
-data = [new_track_times, new_analysis_times, quocients]
-quocients.sort()
+data = [new_track_times, new_analysis_times, quotients]
+quotients.sort()
 
-print ("Mean: " + str(np.mean(quocients[quocients.index(np.percentile(quocients, 25)):quocients.index(np.percentile(quocients, 75))])))
-print ("Median: " + str(np.median(quocients)))
-print ("Second quartile: From " + str(np.percentile(quocients, 25)) + " to " + str(np.percentile(quocients, 50)))
-print ("Third quartile: From " + str(np.percentile(quocients, 50)) + " to " + str(np.percentile(quocients, 75)))
+print ("Mean: " + str(np.mean(quotients[quotients.index(np.percentile(quotients, 25)):quotients.index(np.percentile(quotients, 75))])))
+print ("Median: " + str(np.median(quotients)))
+print ("Second quartile: From " + str(np.percentile(quotients, 25)) + " to " + str(np.percentile(quotients, 50)))
+print ("Third quartile: From " + str(np.percentile(quotients, 50)) + " to " + str(np.percentile(quotients, 75)))
 
 plot2 = plt.figure(2)
 plt.boxplot(data)
-plt.xticks([1,2,3], ["Analysis", "Additional", "Quocients"])
+plt.xticks([1,2,3], ["Analysis", "Additional", "Quotients"])
 plt.title ("Box plots")
 plt.show()
 
