7250b78f9be34ae088da7992b75b0501bbff26ec
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-12-13 12:47:06 +0100

    Add information and adjusted plots

--- CORRELATION/correlation.py
+++ CORRELATION/correlation.py
@@ -16,7 +16,7 @@ new_track_times = []
 new_analysis_times = []
 quocients = []
 
-for i in range(3500):
+for i in range(3501):
 	new_track_times.append(int(track_times[i]))
 	new_analysis_times.append(int(analysis_times[i]))
 	quocients.append(int(track_times[i])/int(analysis_times[i]))
@@ -27,6 +27,12 @@ plt.plot(quocients, 'b*')
 plt.axis('equal')
 
 data = [new_track_times, new_analysis_times, quocients]
+quocients.sort()
+
+print ("Mean: " + str(np.mean(quocients[quocients.index(np.percentile(quocients, 25)):quocients.index(np.percentile(quocients, 75))])))
+print ("Median: " + str(np.median(quocients)))
+print ("Second quartile: From " + str(np.percentile(quocients, 25)) + " to " + str(np.percentile(quocients, 50)))
+print ("Third quartile: From " + str(np.percentile(quocients, 50)) + " to " + str(np.percentile(quocients, 75)))
 
 plot2 = plt.figure(2)
 plt.boxplot(data)
