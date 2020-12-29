1fec75ba610348ddc447eaa7313761b18d96c0f8
Miguel Sánchez <miguelsanchez1703@gmail.com>
2020-12-29 13:37:21 +0100

    Create audioTrack variable and thread

--- ANCMEDIAPLAYER/app/src/main/java/com/miguelsanchezp/ancmediaplayer/MANC.java
+++ ANCMEDIAPLAYER/app/src/main/java/com/miguelsanchezp/ancmediaplayer/MANC.java
@@ -1,18 +1,26 @@
 package com.miguelsanchezp.ancmediaplayer;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+
 public class MANC {
     private static double frequency;
     private static double phase;
     private static final int sampleRate = 44100;
+    private static final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, 8000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, 8000, AudioTrack.MODE_STREAM);
 
     public static Runnable generateFrequency = new Runnable() {
         @Override
         public void run() {
 //            byte[]
+//            audioTrack.write()
         }
     };
 
     static void play () {
+        audioTrack.play();
+        new Thread(generateFrequency).start();
     }
 
     static void setFrequency (double frequency) {
